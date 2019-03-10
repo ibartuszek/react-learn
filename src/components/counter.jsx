c358b9b7c8dfc3b705cbc36e4eb849254279d3c1
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2019-03-10 15:25:46 +0100

    Added h1 with button as fragment

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    React.createElement("div");
+    return (
+      <React.Fragment>
+        <h1>Hello world</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
