4740ae0103b36e928b8bc5b8767c652bb88b712f
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2019-03-10 17:47:16 +0100

    counters render children props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,6 +16,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
